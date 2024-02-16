679ce79facb8dd7aedf559fb4e60cd6d68b26b2d
Devanshi Patel <devanshipatel2611@gmail.com>
2024-02-16 16:34:36 +0530

    admin-user-16.24-adminpanel

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -33,43 +33,43 @@ import withRouter from "../../Components/Common/withRouter";
 import axios from "axios";
 
 const initialState = {
-  Email: "",
-  Password: "",
+  email: "",
+  password: "",
 };
 
 const Login = (props) => {
-  const dispatch = useDispatch();
-  const { user } = useSelector((state) => ({
-    user: state.Account.user,
-  }));
+  // const dispatch = useDispatch();
+  // const { user } = useSelector((state) => ({
+  //   user: state.Account.user,
+  // }));
 
-  const validation = useFormik({
-    enableReinitialize: true,
+  // const validation = useFormik({
+  //   enableReinitialize: true,
 
-    initialValues: {
-      // email: userLogin.email || "abc@gmail.com" || "",
-      // password: userLogin.password || "password" || "",
-      email: "",
-      password: "",
-    },
-    validationSchema: Yup.object({
-      email: Yup.string().required("Please Enter Your Email"),
-      password: Yup.string().required("Please Enter Your Password"),
-    }),
-    onSubmit: (values) => {
-      dispatch(loginUser(values, props.router.navigate));
-    },
-  });
+  //   initialValues: {
+  //     // email: userLogin.email || "abc@gmail.com" || "",
+  //     // password: userLogin.password || "password" || "",
+  //     email: "",
+  //     password: "",
+  //   },
+  //   validationSchema: Yup.object({
+  //     email: Yup.string().required("Please Enter Your Email"),
+  //     password: Yup.string().required("Please Enter Your Password"),
+  //   }),
+  //   onSubmit: (values) => {
+  //     dispatch(loginUser(values, props.router.navigate));
+  //   },
+  // });
 
   const { error } = useSelector((state) => ({
     error: state.Login.error,
   }));
 
-  const [showError, setShowError] = useState(false);
+  // const [showError, setShowError] = useState(false);
   const [values, setValues] = useState(initialState);
   const [formErrors, setFormErrors] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
-  const { Email, Password } = values;
+  const { email, password } = values;
   const [showPassword, setShowPassword] = useState(false);
 
   const handleChange = (e) => {
@@ -105,20 +105,20 @@ const Login = (props) => {
   const validate = (values) => {
     const errors = {};
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (values.Email === "") {
-      errors.Email = "Email is required!";
+    if (values.email === "") {
+      errors.email = "Email is required!";
       seterrEmail(true);
-    } else if (!regex.test(values.Email)) {
-      errors.Email = "Invalid Email address!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "Invalid Email address!";
       seterrEmail(true);
     } else {
       seterrEmail(false);
     }
-    if (values.Password === "") {
-      errors.Password = "Password is required!";
+    if (values.password === "") {
+      errors.password = "Password is required!";
       setErrPassword(true);
     }
-    if (values.Password !== "") {
+    if (values.password !== "") {
       setErrPassword(false);
     }
     return errors;
@@ -172,73 +172,73 @@ const Login = (props) => {
                     <Alert color="danger"> {error} </Alert>
                   ) : null}
                   <div className="p-2 mt-4">
-                    <Form
+                    {/* <Form
                       onSubmit={(e) => {
                         e.preventDefault();
                         validation.handleSubmit();
                         return false;
                       }}
                       action="#"
-                    >
-                      <div className="mb-3">
-                        <Label htmlFor="email" className="form-label">
-                          Email
-                        </Label>
+                    > */}
+                    <div className="mb-3">
+                      <Label htmlFor="email" className="form-label">
+                        Email
+                      </Label>
+                      <Input
+                        name="email"
+                        className={validClassEmail}
+                        placeholder="Enter email"
+                        type="email"
+                        onChange={handleChange}
+                        value={email}
+                      />
+                      {isSubmit && (
+                        <p className="text-danger">{formErrors.email}</p>
+                      )}
+                    </div>
+
+                    <div className="mb-3">
+                      <Label className="form-label" htmlFor="password-input">
+                        Password
+                      </Label>
+                      <div className="position-relative auth-pass-inputgroup mb-3">
                         <Input
-                          name="Email"
-                          className={validClassEmail}
-                          placeholder="Enter email"
-                          type="email"
+                          name="password"
+                          value={password}
+                          type={showPassword ? "text" : "Password"}
+                          className={validClassPassword}
+                          placeholder="Enter Password"
                           onChange={handleChange}
-                          value={Email}
                         />
                         {isSubmit && (
-                          <p className="text-danger">{formErrors.Email}</p>
+                          <p className="text-danger">{formErrors.password}</p>
                         )}
-                      </div>
-
-                      <div className="mb-3">
-                        <Label className="form-label" htmlFor="password-input">
-                          Password
-                        </Label>
-                        <div className="position-relative auth-pass-inputgroup mb-3">
-                          <Input
-                            name="Password"
-                            value={Password}
-                            type={showPassword ? "text" : "Password"}
-                            className={validClassPassword}
-                            placeholder="Enter Password"
-                            onChange={handleChange}
-                          />
-                          {isSubmit && (
-                            <p className="text-danger">{formErrors.Password}</p>
-                          )}
-                          <button
-                            className="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted"
-                            type="button"
-                            id="password-addon"
-                            onClick={() => setShowPassword(!showPassword)}
-                          >
-                            {showPassword ? (
-                              <i class="ri-eye-off-fill  align-middle"></i>
-                            ) : (
-                              <i className="ri-eye-fill align-middle"></i>
-                            )}
-                          </button>
-                        </div>
-                      </div>
-
-                      <div className="mt-4">
-                        <Button
-                          color="success"
-                          className="btn btn-success w-100"
-                          type="submit"
-                          onClick={login}
+                        <button
+                          className="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted"
+                          type="button"
+                          id="password-addon"
+                          onClick={() => setShowPassword(!showPassword)}
                         >
-                          Sign In
-                        </Button>
+                          {showPassword ? (
+                            <i class="ri-eye-off-fill  align-middle"></i>
+                          ) : (
+                            <i className="ri-eye-fill align-middle"></i>
+                          )}
+                        </button>
                       </div>
-                    </Form>
+                    </div>
+
+                    <div className="mt-4">
+                      <Button
+                        color="success"
+                        className="btn btn-success w-100"
+                        type="submit"
+                        onClick={login}
+                      >
+                        Sign In
+                      </Button>
+                    </div>
+                    {/* </Form> */}
                   </div>
                 </CardBody>
               </Card>
